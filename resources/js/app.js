4d6efc53b405358836e1eb18f4fa1b50f9126792
andrew malinnikov <code.bright.anywhere@gmail.com>
2024-01-11 18:33:37 +0500

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,26 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import Layout from '@/layouts/Layout.vue'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    console.log({ pages })
-    let page = pages[`./Pages/${name}.vue`]
-    console.log({ page })
-    page.default.layout = page?.default?.layout || Layout
-    return page
-  },
+  resolve: name =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob('./Pages/**/*.vue'),
+    ).then(page => {
+      page.default.layout = page?.default?.layout || Layout
+      return page
+    }),
+
+  // resolve: name => {
+  //   const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+  //   console.log({ pages })
+  //   let page = pages[`./Pages/${name}.vue`]
+  //   console.log({ page })
+  // page.default.layout = page?.default?.layout || Layout
+  //   return page
+  // },
   setup({ el, App, props, plugin }) {
     const vueApp = createApp({ render: () => h(App, props) })
     vueApp.use(plugin)
