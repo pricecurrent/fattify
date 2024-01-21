9df4cf7a31ce7234914e52bc8534ac29117203b4
andrew malinnikov <code.bright.anywhere@gmail.com>
2024-01-21 23:22:12 +0500

    colors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
         <div class="mt-10 flex items-center gap-x-6">
           <a
             href="{{ route('register') }}"
-            class="rounded-md bg-lime-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-400"
+            class="rounded-md bg-lime-300 px-3.5 py-2.5 text-sm font-semibold text-stone-900 shadow-sm hover:bg-lime-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-400"
           >Get started</a>
           <a
             href="#"
