afce88d6ed134f3cc31f269d4fd867e5371d196e
andrew malinnikov <code.bright.anywhere@gmail.com>
2024-08-21 09:29:50 +0700

    add invite code input

--- resources/js/pages/Auth/Register.vue
+++ resources/js/pages/Auth/Register.vue
@@ -7,7 +7,7 @@
       <Link class="link" href="/login">Sign In</Link>
     </p>
   </div>
-  <form v-if="invite_code && invite_code === 'BETALIST_FRIEND'" @submit.prevent="form.post('/register')" class="mt-6 pb-8">
+  <form v-if="inviteCode && inviteCode === 'BETALIST_FRIEND'" @submit.prevent="form.post('/register')" class="mt-6 pb-8">
     <div class="space-y-2">
       <div>
         <TextInput v-model="form.name" :error="form.errors.name" label="Name" placeholder="John Doe" />
@@ -26,17 +26,24 @@
       <PrimaryButton type="submit" :disabled="form.processing">Login</PrimaryButton>
     </div>
   </form>
-  <div v-else class="rounded-md bg-orange-50 p-4 mt-4 border border-orange-400">
-    <div class="flex">
-      <div class="flex-shrink-0">
-        <ExclamationTriangleIcon class="h-5 w-5 text-orange-400" aria-hidden="true" />
-      </div>
-      <div class="ml-3">
-        <h3 class="text-sm font-medium text-orange-800">Invalid Invite Code</h3>
-        <div class="mt-2 text-sm text-orange-700">
-          <p>You cannot sign up without a valid invite code.</p>
+
+  <div v-else>
+    <div class="rounded-md bg-orange-50 p-4 mt-4 border border-orange-400">
+      <div class="flex">
+        <div class="flex-shrink-0">
+          <ExclamationTriangleIcon class="h-5 w-5 text-orange-400" aria-hidden="true" />
+        </div>
+        <div class="ml-3">
+          <h3 class="text-sm font-medium text-orange-800">Invalid Invite Code</h3>
+          <div class="mt-2 text-sm text-orange-700">
+            <p>You cannot sign up without a valid invite code.</p>
+          </div>
         </div>
       </div>
+    </div>
+
+    <div className="mt-4">
+      <TextInput type="text" v-model="inviteCode" label="Invite Code" />
     </div>
   </div>
 </template>
@@ -48,6 +55,7 @@ import { useForm } from '@inertiajs/vue3'
 import TextInput from '@/components/Shareable/Input/TextInput.vue'
 import PrimaryButton from '@/components/Shareable/Input/PrimaryButton.vue'
 import { Link, Head } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
 export default {
   layout: AuthLayout,
@@ -55,14 +63,16 @@ export default {
     invite_code: String,
   },
   components: { Head, TextInput, PrimaryButton, Link, ExclamationTriangleIcon },
-  setup() {
+  setup(props) {
+    const inviteCode = ref(props.invite_code)
+
     const form = useForm({
       name: null,
       email: null,
       password: null,
       password_confirmation: null,
     })
-    return { form }
+    return { form, inviteCode }
   },
 }
 </script>
